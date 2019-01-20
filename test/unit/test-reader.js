5b5af422253ab73cbfb1ea918be924ae8f7cfe7b
Curtis Elsasser <curt.elsasser@gmail.com>
2019-01-20 15:10:27 -0500

    zagging - going to include both read and write in same repository: - create a lib and make room for writer - outline for tests - some test data

---
+++ test/unit/test-reader.js
@@ -0,0 +1,13 @@
+/**
+ * User: curtis
+ * Date: 2019-01-20
+ * Time: 15:05
+ */
+
+const assert=require("assert");
+const {midiFileRead}=require("../..");
+
+describe("lib.reader", function() {
+
+});
+
---
+++ test/unit/test-writer.js
@@ -0,0 +1,13 @@
+/**
+ * User: curtis
+ * Date: 2019-01-20
+ * Time: 15:05
+ */
+
+const assert=require("assert");
+const {midiFileWrite}=require("../..");
+
+describe("lib.writer", function() {
+
+});
+
